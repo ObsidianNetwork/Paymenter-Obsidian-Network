e9707384a7ea8b012ec4e26f0a5e525b47eaa6cc
Jordan <164892154+Jordanmuss99@users.noreply.github.com>
2026-08-18 06:12:07 +1000

    test: eliminate factory email collisions

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 /**
  * @extends Factory<User>
@@ -26,7 +27,7 @@ class UserFactory extends Factory
         return [
             'first_name' => fake()->name(),
             'last_name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'email' => Str::uuid() . '@example.test',
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
         ];
